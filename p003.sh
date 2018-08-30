f38f87ecd4e2ade5fc6787234f9830aa27723355
Hajime Edakawa <hajime.edakawa@gmail.com>
2018-08-30 22:54:45 +0900

    Initial revision

---
+++ p003.sh
@@ -0,0 +1,19 @@
+#!/bin/ksh
+
+# $Id: p003.sh,v 1.1 2018/08/30 13:53:11 hajime Exp $
+
+# Project Euler #3
+# Hajime Edakawa <hajime.edakawa@gmail.com>
+# Public Domain
+
+integer pf i=2 n=600851475143
+while (( $n > 1 )); do
+	if (( $n % $i == 0 )); then
+		let n/=$i
+		pf=$i
+	else
+		let i++
+	fi
+done
+
+echo $pf
