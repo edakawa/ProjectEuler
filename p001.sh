027495833d683b59bc4d505dfdaa92fa0f7c3d16
Hajime Edakawa <hajime.edakawa@gmail.com>
2018-09-03 00:00:50 +0900

    Convert spaces to tabs

--- p001.sh
+++ p001.sh
@@ -1,6 +1,6 @@
 #!/bin/ksh
 
-# $Id: p001.sh,v 1.1.1.1 2018/08/28 14:08:23 hajime Exp $
+# $Id: p001.sh,v 1.2 2018/09/02 15:00:28 hajime Exp $
 
 # Project Euler #1
 # Hajime Edakawa <hajime.edakawa@gmail.com>
@@ -8,11 +8,11 @@
 
 integer sum=0
 for i in $(jot 999); do
-    if (( i % 3 == 0 )); then
-        let sum+=$i
-    elif (( i % 5 == 0 )); then
-        let sum+=$i
-    fi
+	if (( i % 3 == 0 )); then
+		let sum+=$i
+	elif (( i % 5 == 0 )); then
+		let sum+=$i
+	fi
 done
 
 echo $sum
