c1a0a76ac38f607992f6e35045d6aed784c4de29
Hajime Edakawa <hajime.edakawa@gmail.com>
2018-09-02 23:58:08 +0900

    Convert spaces to tabs

--- p004.sh
+++ p004.sh
@@ -1,6 +1,6 @@
 #!/bin/ksh
 
-# $Id: p004.sh,v 1.1 2018/09/02 14:49:08 hajime Exp $
+# $Id: p004.sh,v 1.2 2018/09/02 14:57:30 hajime Exp $
 
 # Project Euler #4
 # Hajime Edakawa <hajime.edakawa@gmail.com>
@@ -10,20 +10,19 @@ perl -nle 'local $|=1; print scalar reverse $_' |&
 
 let i=1 buf=0 max=0
 while (( $i <= 999 )); do
-    let j=$i
-    while (( $j <= 999 )); do
-        let buf=$i*$j
-        print -p $buf
-	read -p rev
-        if [[ $buf = $rev ]]; then
-            if (( $buf > $max )); then
-                let max=$buf
-            fi
-        fi
-
-        let j+=1
-    done
-    let i+=1
+	let j=$i
+	while (( $j <= 999 )); do
+		let buf=$i*$j
+		print -p $buf
+		read -p rev
+		if [[ $buf = $rev ]]; then
+			if (( $buf > $max )); then
+				let max=$buf
+			fi
+		fi
+		let j+=1
+	done
+	let i+=1
 done
 
 echo $max
