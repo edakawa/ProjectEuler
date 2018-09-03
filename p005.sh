9b059168e4a524fb11e788d543b032620d8b73a5
Hajime Edakawa <hajime.edakawa@gmail.com>
2018-09-04 00:23:44 +0900

    Initial revision

---
+++ p005.sh
@@ -0,0 +1,22 @@
+#!/bin/ksh
+
+# $Id: p005.sh,v 1.1 2018/09/03 15:23:04 hajime Exp $
+
+# Project Euler #5
+# Hajime Edakawa <hajime.edakawa@gmail.com>
+# Public Domain
+
+UPPER=20
+
+set -A se $(jot $UPPER)
+integer i=$(($UPPER-2)) j=$(($UPPER-1))
+while (( 0 < $i )); do
+	integer n=${se[$i]} m=${se[$j]}
+	while (( 0 < n )); do
+		let n=$m%$n m=$n
+	done
+
+	let se[i]=$((se[i]*se[j]/m)) i-- j--
+done
+
+echo ${se[1]}
