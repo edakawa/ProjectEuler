d32e9c1353f7e1cb247b0646f6dd6921c4d5e3a6
Hajime Edakawa <hajime.edakawa@gmail.com>
2018-09-04 00:29:11 +0900

    Remove dollar sign

--- p002.sh
+++ p002.sh
@@ -1,14 +1,14 @@
 #!/bin/ksh
 
-# $Id: p002.sh,v 1.1 2018/08/28 23:23:54 hajime Exp $
+# $Id: p002.sh,v 1.2 2018/09/03 15:28:49 hajime Exp $
 
 # Project Euler #2
 # Hajime Edakawa <hajime.edakawa@gmail.com>
 # Public Domain
 
 integer fib1=1 fib2=2 sum=0
-while (( $fib1 < 4000000 )); do
-	if (( ($fib1 & 1) == 0 )); then
+while (( fib1 < 4000000 )); do
+	if (( (fib1 & 1) == 0 )); then
 		let sum+=$fib1
 	fi
 	let fib1=$fib2 fib2+=$fib1
