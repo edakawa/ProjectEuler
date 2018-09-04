0609c6322180e3045fc5ec3db2301f2050caa2ec
Hajime Edakawa <hajime.edakawa@gmail.com>
2018-09-05 00:50:45 +0900

    Initial revision

---
+++ p007.sh
@@ -0,0 +1,42 @@
+#!/bin/ksh
+
+# $Id: p007.sh,v 1.1 2018/09/04 13:51:11 hajime Exp $
+
+# Project Euler #7
+# Hajime Edakawa <hajime.edakawa@gmail.com>
+# Public Domain
+
+UPPER=10001
+set -A PRIMES 2 3
+
+bc |&
+
+function is_prime {
+	local n=$1 root
+
+	print -p "sqrt($n)"
+	read -p root
+
+	for p in ${PRIMES[*]}; do
+		if (( p > root )); then
+			break
+		fi
+		if (( n % p == 0 )); then
+			return 1
+		fi
+	done
+
+	PRIMES[${#PRIMES[*]}]=$n
+	return 0
+}
+
+integer found=2 n=5 ret
+while (( found < $UPPER )); do
+	is_prime $n
+	if [[ $? -eq 0 ]]; then
+		let found++
+	fi
+	let n+=2
+done
+
+echo ${PRIMES[$((UPPER-1))]}
