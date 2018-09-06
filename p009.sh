a52261d4ab7b6271fd1cb98d9370a2a0173f90fb
Hajime Edakawa <hajime.edakawa@gmail.com>
2018-09-07 00:03:42 +0900

    Add statement to check out-of-range value

--- p009.sh
+++ p009.sh
@@ -1,6 +1,6 @@
 #!/bin/ksh
 
-# $Id: p009.sh,v 1.1 2018/09/06 14:39:48 hajime Exp $
+# $Id: p009.sh,v 1.2 2018/09/06 15:03:30 hajime Exp $
 
 # Project Euler #9
 # Hajime Edakawa <hajime.edakawa@gmail.com>
@@ -10,6 +10,9 @@ integer i=1 j=2 k
 while (( i < 332 )); do
 	let j=i+1
 	while (( i+j < 665 )); do
+		if (( i+j+j+1 > 1000 )); then
+			break
+		fi
 		let k=1000-i-j
 		if (( i*i+j*j == k*k )); then
 			break 2
