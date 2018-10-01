bbc4a9f4d3633ff63c88d1a1686e5bbf9329d8bb
Hajime Edakawa <hajime.edakawa@gmail.com>
2018-10-02 02:55:29 +0900

    Initial revision

---
+++ p025.sh
@@ -0,0 +1,20 @@
+#!/bin/ksh
+
+# $Id: p025.sh,v 1.1 2018/10/01 17:55:17 hajime Exp $
+
+# Project Euler #25
+# Hajime Edakawa <hajime.edakawa@gmail.com>
+# Public Domain
+
+bc -e "
+upper = 10^999
+define fib(f1, f2, depth) {
+	if (f1 >= upper) return depth
+	return fib(f2, f1+f2, depth+1)
+}
+" |&
+
+print -p "fib(1, 1, 1)"
+read -p solution
+
+echo solution
